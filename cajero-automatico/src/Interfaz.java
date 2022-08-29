0d0b39d70b8e4e1887c1627dbbcb169fdf595d67
Azucena Ceniceros <azucg99@gmail.com>
2022-08-28 22:18:52 -0500

    User messages printed

--- cajero-automatico/src/Interfaz.java
+++ cajero-automatico/src/Interfaz.java
@@ -2,6 +2,11 @@
 public class Interfaz {
 
 	public static void main(String[] args) {
+		System.out.println("Escriba 1 para consulta de saldo");
+		System.out.println("Escriba 2 para ingresar dinero");
+		System.out.println("Escriba 3 para sacar dinero");
+		System.out.println("Escriba 4 para consultar movimientos");
+		
 		
 
 	}
