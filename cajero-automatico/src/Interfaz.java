759835ce7bac0908193594e0a8b2d2f78d8e64c4
Azucena Ceniceros <azucg99@gmail.com>
2022-08-30 19:22:44 -0500

    Language changed to english

--- cajero-automatico/src/Interfaz.java
+++ cajero-automatico/src/Interfaz.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Interfaz {
 
 	public static void main(String[] args) {
-		System.out.println("Escriba 1 para consulta de saldo");
-		System.out.println("Escriba 2 para ingresar dinero");
-		System.out.println("Escriba 3 para sacar dinero");
-		System.out.println("Escriba 4 para consultar movimientos");
+		System.out.println("Type 1 for balance inquiry");
+		System.out.println("Type 2 to enter money");
+		System.out.println("Type 3 to withdraw money");
+		System.out.println("Type 4 to check moves");
+		System.out.println("To exit type any other number");
 		
-		Scanner entrada=new Scanner(System.in);
-		System.out.println("¿Qué opción desea elegir?");
-		int opcionElegida= entrada.nextInt(); 
-		System.out.println("Tu opción es la número: " + opcionElegida);
+		Scanner input=new Scanner(System.in);
+		System.out.println("Which option would you like to choose?");
+		int choosenNumber= input.nextInt(); 
+		System.out.println("Tu opción es la número: " + choosenNumber);
 		
 	}
 
