26d86dd457145b38b626c3e8bf10ced8c8871d35
Azucena Ceniceros <azucg99@gmail.com>
2022-08-30 19:47:22 -0500

    The options are repeated until the user decides to exit

--- cajero-automatico/src/Interfaz.java
+++ cajero-automatico/src/Interfaz.java
@@ -3,17 +3,43 @@ import java.util.Scanner;
 public class Interfaz {
 
 	public static void main(String[] args) {
-		System.out.println("Type 1 for balance inquiry");
-		System.out.println("Type 2 to enter money");
-		System.out.println("Type 3 to withdraw money");
-		System.out.println("Type 4 to check moves");
-		System.out.println("To exit type any other number");
+		int chosenNumber;
+		do {
+			System.out.println("Type 1 for balance inquiry");
+			System.out.println("Type 2 to enter money");
+			System.out.println("Type 3 to withdraw money");
+			System.out.println("Type 4 to check moves");
+			System.out.println("To exit type any other number");
+			
+			System.out.println("---Please type the option you need and press Enter---");
+			Scanner input=new Scanner(System.in);
+			chosenNumber= input.nextInt(); 
 		
-		Scanner input=new Scanner(System.in);
-		System.out.println("Which option would you like to choose?");
-		int choosenNumber= input.nextInt(); 
-		System.out.println("Tu opción es la número: " + choosenNumber);
-		
+			//System.out.println("You chose number: " + chosenNumber);
+			
+			switch(chosenNumber) {
+			case 1:
+				System.out.println("You chose: balance inquiry");
+			break;
+			
+			case 2:
+				System.out.println("You chose: enter money");
+			break;
+			
+			case 3:
+				System.out.println("You chose: withdraw money");
+			break;
+			
+			case 4:
+				System.out.println("You chose: check moves");
+			break;
+			
+			default:
+				System.out.println("You left the system, come back soon!");
+			
+			}
+		}while(chosenNumber==1||chosenNumber==2||chosenNumber==3||chosenNumber==4);
+			
 	}
 
 }
